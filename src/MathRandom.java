b5317c142b18b6ee7e94919c171d8ef60bda64c5
reginald <reginald.odhiambo@gmail.com>
2018-12-13 18:15:07 +0300

    Test Commit

---
+++ src/MathRandom.java
@@ -0,0 +1,6 @@
+public class MathRandom {
+    public static void main(String[] args) {
+        int num1=(int)(Math.random() * 100);
+        System.out.println(num1);
+    }
+}
--- src/com/company/Randomizer.java
+++ src/com/company/Randomizer.java
@@ -1,4 +1,23 @@
 package com.company;
+import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class Randomizer {
-}
+    public static void main(String[] args) {
+
+        int number1 = (int)(System.currentTimeMillis() % 100);
+        int number2 = (int)(System.currentTimeMillis() / 7 % 100);
+
+
+        Scanner input =new Scanner(System.in);
+        System.out.println("What is "+number1+" + "+number2+" = ");
+        int answer=input.nextInt();
+
+
+        if (number1+number2==answer){
+            System.out.println("Correct, you are a genius! ");
+        }
+        else{
+            System.out.println("Wrong answer, try again.");
+        }
+}}
