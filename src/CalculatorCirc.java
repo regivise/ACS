1dfb1bd130476e0f86c104d83d1b41b826bd7f2a
reginald <reginald.odhiambo@gmail.com>
2019-04-03 18:27:23 +0300

    Test Commit

---
+++ src/CalculatorCirc.java
@@ -0,0 +1,2 @@
+public class CalculatorCirc {
+}
--- src/MathRandom.java
+++
@@ -1,6 +0,0 @@
-public class MathRandom {
-    public static void main(String[] args) {
-        int num1=(int)(Math.random() * 100);
-        System.out.println(num1);
-    }
-}
