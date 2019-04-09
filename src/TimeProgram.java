b4b519d32d2d1d23138561bfc3313d55569ba225
reginald <reginald.odhiambo@gmail.com>
2019-04-09 19:48:03 +0300

    Source

---
+++ src/TimeProgram.java
@@ -0,0 +1,16 @@
+public class TimeProgram {
+    public static void main(String[] args) {
+        Time shiko= new Time();
+        Time mike = new Time(2,2,3);
+        Time jeff = new Time(3,3,8);
+
+
+        mike.calc();
+        jeff.calc();
+        shiko.calc();
+
+
+
+    }
+
+}
